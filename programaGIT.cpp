cc84adac47464bbb1aadb663fff2124e1f247aba
dudagithub <57270809+dudagithub@users.noreply.github.com>
2019-11-20 16:32:17 -0300

    Add files via upload
    Adicionei 1 função.

--- programaGIT.cpp
+++ programaGIT.cpp
@@ -10,3 +10,14 @@ int contaLetras(char letra, string texto,int totletras)
   }
   return qtd;
 }
+void substituirLetra(char letra1,char letra2, string texto,int totletras)
+{
+  int i;
+  for (i=0;i<totletras;i++)
+  {
+    if(texto[i]==letra1)
+    {
+      texto[i]=letra2;
+    }
+  }
+}
